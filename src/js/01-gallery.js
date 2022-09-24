d7c963a2cec1649e1a7a6338fb6200dfe7721b50
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-24 23:32:55 +0200

    Updete homework 08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galery = document.querySelector('.gallery');
+
+galery.insertAdjacentHTML('beforeend', galeryCreate(galleryItems));
+
+function galeryCreate(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li>
+  <a class="gallery__item" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
