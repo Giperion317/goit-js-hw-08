c9e8c8b3e28fa9ef98ad86215ed9d6f6734a59c9
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-26 02:25:35 +0200

    Update task 02 - video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galery = document.querySelector('.gallery');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
+
+const parsedCurentTimeVideo = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player.setCurrentTime(parsedCurentTimeVideo.seconds);
