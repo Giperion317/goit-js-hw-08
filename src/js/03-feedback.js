2741e23866c25e5362ac1a5d637139b9f6d8cc30
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-27 02:29:50 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+form.addEventListener('submit', onFormSubmit);
+
+populateTextareaInput();
+
+function onFormInput() {
+   const formData = {
+    mail: `${input.value}`,
+    message: `${textarea.value}`,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextareaInput() {
+  const parsedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parsedFormData) {
+    input.value = parsedFormData.mail;
+    textarea.value = parsedFormData.message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const submitFormData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(submitFormData);
+  event.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
