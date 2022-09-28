a2661a76643c2595f834d527f95f5084cb5ac095
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-28 13:26:28 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
+const STORAGE_KEY = 'videoplayer-current-time';
 
 document.addEventListener('DOMContentLoaded', onReloadPage);
 
@@ -10,10 +11,25 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
 
 function onPlayerTimeupdate(data) {
-  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+  localStorage.setItem(STORAGE_KEY, `${data.seconds}`);
 }
 
 function onReloadPage() {
-  const playerTime = localStorage.getItem('videoplayer-current-time');
-  player.setCurrentTime(playerTime);
+  const playerTime = localStorage.getItem(STORAGE_KEY);
+  player
+    .setCurrentTime(playerTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
 }
