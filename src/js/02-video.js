9e8ece1ac2b10fe7322fd610e6bf127a3fb6f2b0
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-29 00:14:00 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,33 +4,23 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const STORAGE_KEY = 'videoplayer-current-time';
+const playerTime = localStorage.getItem(STORAGE_KEY);
 
 document.addEventListener('DOMContentLoaded', onReloadPage);
 
 const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
 
-function onPlayerTimeupdate(data) {
-  localStorage.setItem(STORAGE_KEY, `${data.seconds}`);
+function onPlayerTimeupdate({ seconds, duration }) {
+  localStorage.setItem(STORAGE_KEY, `${seconds}`);
+  if (duration === seconds) {
+    localStorage.removeItem(STORAGE_KEY);
+  }
 }
 
-function onReloadPage(event) {
-  const playerTime = localStorage.getItem(STORAGE_KEY);
-  player
-    .setCurrentTime(playerTime)
-    .then(function (seconds) {
-      player.setVolume(0);
-      player.play();
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
-
-        default:
-          // some other error occurred
-          break;
-      }
-    });
+function onReloadPage() {
+  player.setCurrentTime(playerTime || 0).then(function (seconds) {
+    player.setVolume(0);
+    player.play();
+  });
 }
